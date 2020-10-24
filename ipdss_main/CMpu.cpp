0b5e0a9be49c05ced0a9fb753220be219a9fe572
5FB5 <vecktor911@gmail.com>
2020-10-24 20:10:13 +0300

    added mpu code to cmpu class (test)

--- ipdss_main/CMpu.cpp
+++ ipdss_main/CMpu.cpp
@@ -3,49 +3,17 @@
 //#define OUTPUT_READABLE_EULER
 //#define OUTPUT_READABLE_REALACCEL
 //#define OUTPUT_READABLE_WORLDACCEL
-
 #define SERIAL_BUD_SPEED 115200
-
 #define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
 
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
+#include "CMpu.h"
 
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
-#endif
-
-MPU6050 mpu;
-
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-
-void dmpDataReady() {
+void CMpu::dmpDataReady() {
     mpuInterrupt = true;
 }
 
-void setI2cMpu() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
+void CMpu::setMpu() {
+        // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
         Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
@@ -58,42 +26,42 @@ void setI2cMpu() {
 
     // initialize device
     Serial.println(F("Initializing I2C devices..."));
-    mpu.initialize();
+    mpu->initialize();
     pinMode(INTERRUPT_PIN, INPUT);
 
     // verify connection
     Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    Serial.println(mpu->testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
     // load and configure the DMP
     Serial.println(F("Initializing DMP..."));
-    devStatus = mpu.dmpInitialize();
+    devStatus = mpu->dmpInitialize();
 
     // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+    mpu->setXGyroOffset(220);
+    mpu->setYGyroOffset(76);
+    mpu->setZGyroOffset(-85);
+    mpu->setZAccelOffset(1788); // 1688 factory default for my test chip
 
     // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // turn on the DMP, now that it's ready
         Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
+        mpu->setDMPEnabled(true);
 
         // enable Arduino interrupt detection
         Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
         Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
         Serial.println(F(")..."));
-        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING); // TODO: place dmpDataReady in sketch
+        mpuIntStatus = mpu->getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
         Serial.println(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
 
         // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
+        packetSize = mpu->dmpGetFIFOPacketSize();
     } else {
         // ERROR!
         // 1 = initial memory load failed
@@ -105,11 +73,7 @@ void setI2cMpu() {
     }
 }
 
-void setup() {
-    setI2cMpu();
-}
-
-void loop() {
+void CMpu::processMpu() {
     // if programming failed, don't try to do anything
     if (!dmpReady) return;
 
@@ -117,16 +81,16 @@ void loop() {
     while (!mpuInterrupt && fifoCount < packetSize) {
         if (mpuInterrupt && fifoCount < packetSize) {
           // try to get out of the infinite loop 
-          fifoCount = mpu.getFIFOCount();
+          fifoCount = mpu->getFIFOCount();
         }  
     }
     
     // reset interrupt flag and get INT_STATUS byte
     mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
+    mpuIntStatus = mpu->getIntStatus();
 
     // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
+    fifoCount = mpu->getFIFOCount();
 	if(fifoCount < packetSize){
 	        //Lets go back and wait for another interrupt. We shouldn't be here, we got an interrupt from another event
 			// This is blocking so don't do it   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
@@ -134,7 +98,7 @@ void loop() {
     // check for overflow (this should never happen unless our code is too inefficient)
     else if ((mpuIntStatus & (0x01 << MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
         // reset so we can continue cleanly
-        mpu.resetFIFO();
+        mpu->resetFIFO();
       //  fifoCount = mpu.getFIFOCount();  // will be zero after reset no need to ask
         Serial.println(F("FIFO overflow!"));
 
@@ -143,7 +107,7 @@ void loop() {
 
         // read a packet from FIFO
 	while(fifoCount >= packetSize){ // Lets catch up to NOW, someone is using the dreaded delay()!
-		mpu.getFIFOBytes(fifoBuffer, packetSize);
+		mpu->getFIFOBytes(fifoBuffer, packetSize);
 		// track FIFO count here in case there is > 1 packet available
 		// (this lets us immediately read more without waiting for an interrupt)
 		fifoCount -= packetSize;
@@ -164,13 +128,13 @@ void loop() {
 
         #ifdef OUTPUT_READABLE_YAWPITCHROLL
             // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            mpu->dmpGetQuaternion(&q, fifoBuffer);
+            mpu->dmpGetGravity(&gravity, &q);
+            mpu->dmpGetYawPitchRoll(ypr, &q, &gravity);
 
-            float mpuYaw = ypr[0] * 180 / M_PI;
-            float mpuPitch = ypr[1] * 180 / M_PI;
-            float mpuRoll = ypr[2] * 180 / M_PI;
+            mpuYaw = ypr[0] * 180 / M_PI;
+            mpuPitch = ypr[1] * 180 / M_PI;
+            mpuRoll = ypr[2] * 180 / M_PI;
 
             // Don't forget to choose axis
             if (mpuPitch > 0) {
@@ -194,11 +158,11 @@ void loop() {
         #ifdef OUTPUT_READABLE_WORLDACCEL
             // display initial world-frame acceleration, adjusted to remove gravity
             // and rotated based on known orientation from quaternion
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+            mpu->dmpGetQuaternion(&q, fifoBuffer);
+            mpu->dmpGetAccel(&aa, fifoBuffer);
+            mpu->dmpGetGravity(&gravity, &q);
+            mpu->dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            mpu->dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
             Serial.print("aworld\t");
             Serial.print(aaWorld.x);
             Serial.print("\t");
--- pid_test/src/pid_test.ino
+++ pid_test/src/pid_test.ino
@@ -4,10 +4,10 @@
  */
 
 // Motor 1
- #define PIN_MOTOR_ENA 9
- #define PIN_MOTOR_IN1 8
- #define PIN_MOTOR_IN2 7
- #define PIN_MOTOR_INTERRUPT 0
+ #define PIN_MOTOR1_ENA 9
+ #define PIN_MOTOR1_IN1 8
+ #define PIN_MOTOR1_IN2 7
+ #define PIN_MOTOR1_INTERRUPT 0
 
  // Motor 2
  //...
@@ -26,7 +26,8 @@
  #include <Arduino.h>
  #include "CMotor.h"
 
-CMotor* motor1 = new CMotor(PIN_MOTOR_INTERRUPT, PIN_MOTOR_ENA, PIN_MOTOR_IN1, PIN_MOTOR_IN2);
+CMotor* motor1 = new CMotor(PIN_MOTOR1_INTERRUPT, PIN_MOTOR1_ENA, PIN_MOTOR1_IN1, PIN_MOTOR1_IN2);
+// CMotor* motor2 = new CMotor(PIN_MOTOR_INTERRUPT, PIN_MOTOR2_ENA, PIN_MOTOR2_IN1, PIN_MOTOR2_IN2);
 
 /* ============
  *  MAIN
@@ -37,16 +38,28 @@ void increaseMotor1EncoderTicks() { // 'cause it doesn't work in class
    motor1->tickCount++;
 }
 
+/*
+void increaseMotor2EncoderTicks() {
+   motor2->tickCount++;
+}
+*/
+
 void setup() {  
   motor1->setup(MOTOR_REDUCTION_COEFF, T, PID_KP, PID_KI, PID_KD, PID_MIN, PID_MAX);
   attachInterrupt(0, increaseMotor1EncoderTicks, RISING);
+
+  /*
+   motor2->setup(MOTOR_REDUCTION_COEFF, T, PID_KP, PID_KI, PID_KD, PID_MIN, PID_MAX);
+   attachInterrupt(0, increaseMotor2EncoderTicks, RISING);
+  */
   
   Serial.begin(115200); // ??
  }
 
 void loop() {
-   motor1->motorProcess(0, 1); // RPS
+   motor1->motorProcess(1, 1); // RPS
+   //motor2->motorProcess(1, 1);
    
-   Serial.print("Motor RPS: ");
+   Serial.print("Motor1 RPS: ");
    Serial.println(motor1->rvPerS);
 }
